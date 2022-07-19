cbca8df3cf1c3d967ff2ffde8459bd61b71fafe0
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-19 23:33:55 +0100

    add changes to menu js

--- js/menu.js
+++ js/menu.js
@@ -1,40 +1,54 @@
 const hamburger = document.querySelector('.hamburger');
-const ul = document.querySelector('.nav-active')
-const envelope = document.querySelector('.mail')
-const showClose = document.querySelector('.show-close-menu')
+const ul = document.querySelector('.nav-active');
+const envelope = document.querySelector('.mail');
+const showClose = document.querySelector('.show-close-menu');
 const open = document.querySelector('.nav');
 const close = document.querySelector('.close-menu');
 const list = document.querySelectorAll('.nav-links');
 
-let showMenu = () => {
-
+const showMenu = () => {
   open.classList.toggle('nav');
   open.classList.toggle('mobile-menu');
-  ul.classList.toggle('menu')
-  showClose.classList.toggle('menu-img')
-  envelope.style.display = 'none'
+  ul.classList.toggle('menu');
+  showClose.classList.toggle('menu-img');
+  envelope.style.display = 'none';
   list.forEach((item) => {
-    item.classList.toggle('nav-links')
-    item.classList.toggle('menu-text')    
-    });
-}
+    item.classList.toggle('nav-links');
+    item.classList.toggle('menu-text');
+  });
+};
 
-let closeMenu = () => {
+const closeMenu = () => {
   open.classList.toggle('nav');
   open.classList.toggle('mobile-menu');
-  ul.classList.toggle('menu')
-  showClose.classList.toggle('menu-img')
-  envelope.style.display = 'block'
+  ul.classList.toggle('menu');
+  showClose.classList.toggle('menu-img');
+  envelope.style.display = 'block';
   list.forEach((item) => {
-    item.classList.toggle('nav-links')
-    item.classList.toggle('menu-text')    
-    });
-}
+    item.classList.toggle('nav-links');
+    item.classList.toggle('menu-text');
+  });
+};
 
-hamburger.addEventListener('click', showMenu)
+hamburger.addEventListener('click', showMenu);
 
 close.addEventListener('click', closeMenu);
 
 list.forEach((item) => {
-  item.addEventListener('click', closeMenu);
+  item.addEventListener('click', () => {
+    if (open.classList.contains('nav')) {
+      return;
+    }
+
+    open.classList.add('nav');
+
+    open.classList.remove('mobile-menu');
+    ul.classList.remove('menu');
+    showClose.classList.remove('menu-img');
+    envelope.style.display = 'block';
+    list.forEach((item) => {
+      item.classList.toggle('nav-links');
+      item.classList.toggle('menu-text');
+    });
+  });
 });
