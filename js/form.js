8c3bd4aaa7c6c12d8b9138083ed3dbebe4449eb6
Efiamotu-1 <musahabeebefy10@gmail.com>
2022-07-21 23:20:13 +0100

    add form error styles and fix all linter issues

--- js/form.js
+++ js/form.js
@@ -1,18 +1,10 @@
-const form = document.querySelector('form')
-const email = document.querySelector('#email')
-const emailError = document.querySelector('.form-error')
+const form = document.querySelector('form');
+const emailError = document.querySelector('.form-error');
 
-form.addEventListener('submit', (e) => { 
-    e.preventDefault()
-    let name = form.name.value
-    let email = form.email.value
-    let message = form.message.value
-    if (email !== email.toLowerCase()) {
-        emailError.innerHTML = `<p class="error">Email should be written in lowercase letters: ${email}</p>`
-        console.log('error submission')
-    }
-
-    
-})
-
-
+form.addEventListener('submit', (e) => {
+  const email = form.email.value;
+  if (email !== email.toLowerCase()) {
+    e.preventDefault();
+    emailError.innerHTML = `<p class="error">Email should be written in lowercase letters: ${email}</p>`;
+  }
+});
